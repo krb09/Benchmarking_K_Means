80634a014da441871c1792e09295ef80a8eeefa4
krb09 <krb09@js-156-163.jetstream-cloud.org>
2020-09-13 13:17:15 -0400

    new script

--- andre_KMeans.py
+++ andre_KMeans.py
@@ -1,24 +1,27 @@
 from pandas import DataFrame
-#import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import random
 import numpy as np
-from time import time
-import timeit
-import sys
 import time
+import sys
 
-number_of_data_points=int(sys.argv[1])
-number_of_clusters=int(sys.argv[2])
+number_of_data_points=int(sys.argv[2])
+number_of_clusters=int(sys.argv[1])
 def get_random_cluster_points(number_points, number_dim):
     mu = np.random.randn()
     sigma = np.random.randn()
     p = sigma * np.random.randn(number_points, number_dim) + mu
     return p
 
+t1 = time.time()
 p = get_random_cluster_points(number_of_data_points, 2)
+#t1 = time.time()
+kmeans = KMeans(n_clusters=number_of_clusters,n_jobs=1,tol=-0).fit(p)
+#t2 = time.time()
 
-print(time.time())
-kmeans = KMeans(n_clusters=number_of_clusters,n_jobs=-1).fit(p)
-print(time.time())
+#time_avg = t2-t1
+#print('time_total:' + str(time_avg))
 centroids = kmeans.cluster_centers_
+t2 = time.time()
+time_avg = t2-t1
+print('time_total:' + str(time_avg))
